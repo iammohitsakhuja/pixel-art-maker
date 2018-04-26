4da749f7fe1fa5367282be048026202ab2751476
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-04-26 10:07:06 +0530

    Use event delegation

--- designs.js
+++ designs.js
@@ -1,23 +1,18 @@
-let inputHeight;
-let inputWidth;
-let pixelCanvas;
-let cellColor;
-
-pixelCanvas = $('#pixelCanvas');
+const pixelCanvas = $('#pixelCanvas');
 
 $('#sizePicker').on('submit', function(event) {
     // Clear previous canvas, if any
     pixelCanvas.empty();
 
     // Get height and width of the grid
-    inputHeight = parseInt($('#inputHeight').val());
-    inputWidth = parseInt($('#inputWeight').val());
+    let inputHeight = parseInt($('#inputHeight').val());
+    let inputWidth = parseInt($('#inputWeight').val());
 
     // Generate the grid
     makeGrid(inputHeight, inputWidth);
 
-    // Attach event listeners to each cell in the grid
-    attachEventListeners(inputHeight, inputWidth);
+    // Delegate `click` event to all cells
+    pixelCanvas.delegate('td', 'click', changeCellColor);
 
     // Prevent the default action on form submission
     event.preventDefault();
@@ -26,35 +21,19 @@ $('#sizePicker').on('submit', function(event) {
 // Generates the grid
 function makeGrid(inputHeight, inputWidth) {
     for (let i = 0; i < inputHeight; i++) {
-        // Insert a row into the grid and give it a unique ID
-        pixelCanvas.append('<tr id="row-' +
-                            (i + 1).toString() +
-                            '"></tr>');
+        // Insert a row into the grid
+        pixelCanvas.append('<tr></tr>');
 
-        // Select current row
-        let currentRow = $('#row-' + (i + 1).toString());
-
-        // Insert cells into the current row and give each cell a unique ID
+        // Get that row and add cells to it
+        const row = $('#pixelCanvas tr').last();
         for (let j = 0; j < inputWidth; j++) {
-            currentRow.append('<td id="cell-' +
-                            (i + 1).toString() +'-' +
-                            (j + 1).toString() +
-                            '"></td>');
+            const cell = row.append('<td></td>');
         }
     }
 }
 
 // Change the color of the cell that has been clicked
 function changeCellColor() {
-    cellColor = $('#colorPicker').val();
+    let cellColor = $('#colorPicker').val();
     $(this).css('background-color', cellColor);
-}
-
-// Listen to each cell in the grid for a click
-function attachEventListeners(inputHeight, inputWidth) {
-    for (let i = 0; i < inputHeight; i++) {
-        for (let j = 0; j < inputWidth; j++) {
-            $('#cell-' + (i + 1).toString() + '-' + (j + 1).toString()).on('click', changeCellColor);
-        }
-    }
 }
